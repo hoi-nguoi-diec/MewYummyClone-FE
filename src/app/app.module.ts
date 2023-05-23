6f8f6da2e2e7c8524f9e1fdcb859278a490818c8
Kieu Quang Vinh <vinhkqph25485@fpt.edu.vn>
2023-05-23 17:36:26 +0700

    FP-17 <Trang sản phẩm>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './component/product/product.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
