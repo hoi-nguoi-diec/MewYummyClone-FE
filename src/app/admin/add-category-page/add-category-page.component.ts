402ee731b0d6c0ede48e6601aaedf9c28035eba8
thuanph25425 <tranthuan140623@gmail.com>
2023-05-24 10:04:40 +0700

    FP-19 <gia dien admin>

---
+++ src/app/admin/add-category-page/add-category-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-category-page',
+  templateUrl: './add-category-page.component.html',
+  styleUrls: ['./add-category-page.component.scss']
+})
+export class AddCategoryPageComponent {
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutAdminComponent } from './layout/layout-admin/layout-admin.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { ProductPageComponent } from './admin/product-page/product-page.component';
+import { AddProductPageComponent } from './admin/add-product-page/add-product-page.component';
+import { UpdateProductPageComponent } from './admin/update-product-page/update-product-page.component';
+import { CategoryPageComponent } from './admin/category-page/category-page.component';
+import { AddCategoryPageComponent } from './admin/add-category-page/add-category-page.component';
+import { UpdateCategoryPageComponent } from './admin/update-category-page/update-category-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: LayoutAdminComponent, children: [
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'product', component: ProductPageComponent},
+    { path: 'product/add', component: AddProductPageComponent},
+    { path: 'product/edit', component: UpdateProductPageComponent},
+    { path: 'category', component: CategoryPageComponent},
+    { path: 'category/addd', component: AddCategoryPageComponent},
+    { path: 'category/editt', component: UpdateCategoryPageComponent},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
